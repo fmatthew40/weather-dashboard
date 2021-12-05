1d53e2e18f0fa8bd7493bd5d03c261164380a95b
Matt Foster <fostmatt@Matts-MacBook-Pro.local>
2021-12-04 21:58:23 -0700

    current weather updated

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,7 +2,7 @@
 // searchButton.addEventListener("click", getWeatherData);
 
 
-var apiUrl = "api.openweathermap.org/data/2.5/weather?q=phoenix&appid=eda4403bd9c90cb2bfa5115b8964ea2f"
+var apiUrl = "api.openweathermap.org/data/2.5/weather?q=phoenix&appid=eda4403bd9c90cb2bfa5115b8964ea2f&units=imperial"
 // var searchButton = document.querySelector("#searchButton");
 var searchButton = document.getElementById("searchButton");
 
@@ -16,30 +16,50 @@ var humidity = document.querySelector("#humidity");
 var windSpeed = document.querySelector("#windSpeed");
 var uvIndex = document.querySelector("#uvIndex");
 
-var getWeatherData = function() {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=phoenix&appid=eda4403bd9c90cb2bfa5115b8964ea2f");
-    console.log("data");
-     
-    // fetch("https://api.openweathermap.org/data/2.5/weather?q="+cityinput+"&appid=eda4403bd9c90cb2bfa5115b8964ea2f");
+// var getWeatherData = function() {
+//     fetch("https://api.openweathermap.org/data/2.5/weather?q=phoenix&appid=eda4403bd9c90cb2bfa5115b8964ea2f").then(function(response){
+//     if (response.ok) {
+//         response.json().then(function(data){
+//             // displayWeather(data);
+//             console.log(data);
+//         });
+//     } else {
+//         alert("Could not find city; try again.")
+//     }
+//     });
+// };
+
+
+searchButton.addEventListener("click", function(){
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+cityinput.value+"&appid=eda4403bd9c90cb2bfa5115b8964ea2f&units=imperial").then(function(response){
+    if (response.ok) {
+        response.json().then(function(data){
+        // displayWeather(data);
+        console.log(data);
+            currentDisplay(data);
+        });
+    } else {
+        alert("Could not find city; try again.")
+        }
+    });
+    });
+
+var currentDisplay = function(data) {
+    var nameDisplay = data['name'];
+    cityName.innerText = nameDisplay;
+    $("#date").text(moment().format("dddd, MMMM Do YYYY"));
+    var humidityDisplay = "Humidity: " + data['main']['humidity']+"%";
+    humidity.innerText = humidityDisplay; 
+    var temperatureDisplay = "Temperature: " + data['main']['temp']+"°F";
+    temperature.innerText = temperatureDisplay;
+    var windSpeedDisplay = "Wind Speed: " + data['wind']['speed']+"mph";
+    windSpeed.innerText = windSpeedDisplay; 
+    // Fix
+    var weatherIconDisplay = ['weather']['0'][4];
+    weatherIcon.innerHTML = weatherIconDisplay;
+    
+    var uvIndexDisplay = []
+    uvIndex.innerText = uvIndexDisplay; 
 
 };
-
-getWeatherData();
-// var getWeatherData = function(data) {
-//     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+cityinput+"&appid=eda4403bd9c90cb2bfa5115b8964ea2f";
-//     fetch(apiUrl).then(function(response) {
-//         // successful response
-//         if (response.ok) {
-//             response.json().then(function(data){
-//                 displayCurrentWeather();
-//                 console.log(data);
-//             });
-//         }else {
-//             alert("City Data Not Found");
-//         }
-//     });
-// }
-
-// var displayCurrentWeather = function() {
-
-// };
+    
