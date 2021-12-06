d66c5fb77353befaa2894951e663792471d34130
Matt Foster <fostmatt@Matts-MacBook-Pro.local>
2021-12-05 19:44:41 -0700

    localStorage updated and added save feature

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,4 @@
 
-// searchButton.addEventListener("click", getWeatherData);
-
-
-var apiUrl = "api.openweathermap.org/data/2.5/weather?q=phoenix&appid=eda4403bd9c90cb2bfa5115b8964ea2f&units=imperial"
-// var searchButton = document.querySelector("#searchButton");
 var searchButton = document.getElementById("searchButton");
 
 var cityinput = document.querySelector("#city-search");
@@ -16,33 +11,36 @@ var humidity = document.querySelector("#humidity");
 var windSpeed = document.querySelector("#windSpeed");
 var uvIndex = document.querySelector("#uvIndex");
 
-// var getWeatherData = function() {
-//     fetch("https://api.openweathermap.org/data/2.5/weather?q=phoenix&appid=eda4403bd9c90cb2bfa5115b8964ea2f").then(function(response){
-//     if (response.ok) {
-//         response.json().then(function(data){
-//             // displayWeather(data);
-//             console.log(data);
-//         });
-//     } else {
-//         alert("Could not find city; try again.")
-//     }
-//     });
-// };
-
-
 searchButton.addEventListener("click", function(){
-    fetch("https://api.openweathermap.org/data/2.5/weather?q="+cityinput.value+"&appid=eda4403bd9c90cb2bfa5115b8964ea2f&units=imperial").then(function(response){
+    fetchWeather(cityinput.value);
+});
+
+var fetchWeather = function(city) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=eda4403bd9c90cb2bfa5115b8964ea2f&units=imperial").then(function(response){
     if (response.ok) {
         response.json().then(function(data){
         // displayWeather(data);
         console.log(data);
-            currentDisplay(data);
+        currentDisplay(data);
+
+        var savedCities = localStorage.getItem("cities")
+        savedCities = savedCities ? savedCities.split(',') : [];
+
+        savedCities = savedCities.filter(function(localCity){
+            return localCity.toLowerCase() !== city.toLowerCase();
+        });
+
+        savedCities.push(city);
+
+       localStorage.setItem("cities", savedCities.join(','));
+       loadCities();
         });
     } else {
         alert("Could not find city; try again.")
         }
     });
-    });
+
+}
 
 var currentDisplay = function(data) {
     var nameDisplay = data['name'];
@@ -54,12 +52,60 @@ var currentDisplay = function(data) {
     temperature.innerText = temperatureDisplay;
     var windSpeedDisplay = "Wind Speed: " + data['wind']['speed']+"mph";
     windSpeed.innerText = windSpeedDisplay; 
-    // Fix
-    var weatherIconDisplay = ['weather']['0'][4];
-    weatherIcon.innerHTML = weatherIconDisplay;
-    
-    var uvIndexDisplay = []
-    uvIndex.innerText = uvIndexDisplay; 
+    var imageCode = data.weather[0].icon;
+    weatherIcon.setAttribute("src","http://openweathermap.org/img/w/" + imageCode + ".png")
+   
+    var lon = data.coord.lon;
+    var lat = data.coord.lat;
 
-};
+    uvi(lon, lat);
+
     
+};
+
+var uvi = function(lon, lat) {
+   
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely&appid=eda4403bd9c90cb2bfa5115b8964ea2f").then(function(response) {
+    if (response.ok) {
+    response.json().then(function(datab){
+        console.log(datab)
+        var uvidisplay = datab['current']['uvi'];
+        uvIndex.innerText = "UV Index: " +uvidisplay;
+
+    });
+    }else {
+        console.log("error retrieving");
+    };
+        });
+}; 
+
+
+
+var loadCities = function() {
+
+    var searchResultsArea = document.getElementById("search-results");
+    // var searchButton = document.createElement("button");
+    // console.log(searchButton);
+    // innertext
+    // searchButton.innerText = 
+
+    var cities = localStorage.getItem("cities");
+    cities = cities ? cities.split(",") : [];
+    console.log(cityinput);
+
+
+    // searchResultsArea.appendChild(searchButton);
+    searchResultsArea.innerHTML = "";
+
+    for (i =0; i < cities.length; i++) {
+        var searchButton = document.createElement("button");
+        searchButton.innerText = cities[i];
+        searchButton.addEventListener("click", function(event){
+            console.log(event)
+            fetchWeather(event.target.innerText);
+        });
+        searchResultsArea.appendChild(searchButton);
+    }
+};
+
+loadCities()
